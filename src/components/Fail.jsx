18e7eaa913f567dce2a1178afdc826c76651c438
lakshmanana456 <elakshmanan10501@gmail.com>
2025-07-21 08:19:04 +0530

    dashboard ui

--- src/components/Fail.jsx
+++ src/components/Fail.jsx
@@ -1,6 +1,8 @@
 function Fail(){
     return(
-        <h1>fail</h1>
+         <main className="min-h-screen bg-black text-white px-6 py-8">
+        <h1 className=" font-bold text-3xl text-center">Login failed!</h1>
+   </main>
     )
 }
 export default Fail
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,6 +1,8 @@
 function Success(){
     return(
-        <h1>success</h1>
+         <main className="min-h-screen bg-black text-white px-6 py-8">
+        <h1 className="font-bold text-3xl text-center">Login Successfully 🎉</h1>
+        </main>
     )
 }
 export default Success
